523ba8cc44e52a5e285c97c319b0c59770b4249e
seungwon7934 <sw7934@gmail.com>
2024-04-16 09:21:01 +0900

    first commit

---
+++ src/test/java/gcu/msa/config/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package gcu.msa.config.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
